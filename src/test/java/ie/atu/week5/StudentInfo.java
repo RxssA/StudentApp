d02ea94e5a1507717f2219c7bfb7b00a4b83eb2c
g00401655 <g00401655@atu.ie>
2022-10-24 18:43:11 +0100

    added new file called StudentInfo

---
+++ src/test/java/ie/atu/week5/StudentInfo.java
@@ -0,0 +1,31 @@
+package ie.atu.week5;
+
+public class StudentInfo {
+    private double name;
+    private double email;
+    private double course;
+
+    public double getName() {
+        return name;
+    }
+
+    public void setName(double name) {
+        this.name = name;
+    }
+
+    public double getEmail() {
+        return email;
+    }
+
+    public void setEmail(double email) {
+        this.email = email;
+    }
+
+    public double getCourse() {
+        return course;
+    }
+
+    public void setCourse(double course) {
+        this.course = course;
+    }
+}
